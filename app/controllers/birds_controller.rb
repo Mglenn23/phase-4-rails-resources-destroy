008796c20d542a189abc78ad23c8f5ddc06b2c6d
michael <michaelglenn88@yahoo.com>
2022-08-24 13:35:11 -0400

    Completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,6 +6,17 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
+  # DESTROY
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   # POST /birds
   def create
     bird = Bird.create(bird_params)
